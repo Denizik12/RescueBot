80d1134b094eed4f1d4327dc9f03489d58073d61
denizik12 <denizik12@hotmail.com>
2020-01-04 18:41:50 +0100

    ir sensor code en ultra soon code toegevoegd

--- ir_zonder_esp/ir_zonder_esp.ino
+++ ir_zonder_esp/ir_zonder_esp.ino
@@ -1,12 +1,69 @@
-int irPinLeft = A1;
-int irPinRight = A0;
-int valueLeft = 0;
-int valueRight = 0;
+// ir sensor pins
+#define IR_PIN_LEFT_FRONT A0
+#define IR_PIN_RIGHT_FRONT A1
+#define IR_PIN_LEFT_SIDE A2
+#define IR_PIN_RIGHT_SIDE A3
 
+// ir sensor values
+int valueLeftSideFront = 0;
+int valueRightSideFront = 0;
+int valueLeftSide = 0;
+int valueRightSide = 0;
+
+// h-bridge module pins
 #define PIN_1 2
 #define PIN_2 3
 #define PIN_3 4
 #define PIN_4 5
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(IR_PIN_LEFT_FRONT, INPUT);
+  pinMode(IR_PIN_RIGHT_FRONT, INPUT);
+  pinMode(IR_PIN_LEFT_SIDE, INPUT);
+  pinMode(IR_PIN_RIGHT_SIDE, INPUT);
+}
+
+void loop() {
+
+  valueLeftSideFront = digitalRead(IR_PIN_LEFT_FRONT);
+  valueRightSideFront = digitalRead(IR_PIN_RIGHT_FRONT);
+  valueLeftSide = digitalRead(IR_PIN_LEFT_SIDE);
+  valueRightSide = digitalRead(IR_PIN_RIGHT_SIDE);
+
+  // if all sensors detect nothing go forward
+  if (valueLeftSideFront == 0 && valueRightSideFront == 0 && valueLeftSide == 0 && valueRightSide == 0) {
+    motorDirectionForward();
+
+  } else if (valueRightSideFront == 1 && valueLeftSideFront == 0 && valueLeftSide == 0 && valueRightSide == 0) {
+    motorDirectionLeft();
+
+  } else if (valueRightSideFront == 0 && valueLeftSideFront == 0 && valueLeftSide == 0 && valueRightSide == 1) {
+    motorDirectionLeft();
+
+  } else if (valueRightSideFront == 1 && valueLeftSideFront == 0 && valueLeftSide == 0 && valueRightSide == 1) {
+    motorDirectionLeft();
+
+  } else if (valueRightSideFront == 1 && valueLeftSideFront == 1 && valueLeftSide == 0 && valueRightSide == 1) {
+    motorDirectionLeft();
+
+  } else if (valueRightSideFront == 1 && valueLeftSideFront == 1 && valueLeftSide == 1 && valueRightSide == 0) {
+    motorDirectionRight();
+
+  } else if (valueRightSideFront == 0 && valueLeftSideFront == 1 && valueLeftSide == 1 && valueRightSide == 0) {
+    motorDirectionRight();
+
+  } else if (valueRightSideFront == 0 && valueLeftSideFront == 1 && valueLeftSide == 0 && valueRightSide == 0) {
+    motorDirectionRight();
+
+  } else if (valueRightSideFront == 0 && valueLeftSideFront == 0 && valueLeftSide == 1 && valueRightSide == 0) {
+    motorDirectionRight();
+
+  } else if (valueLeftSide == 1 && valueRightSide == 1 && valueLeftSideFront == 1 && valueRightSideFront == 1) {
+    motorStop();
+  }
+
+}
 
 // motor forward function
 void motorDirectionForward() {
@@ -27,7 +84,7 @@ void motorDirectionBackward() {
 }
 
 // motor turn left function
-void motorDirectionLeft() {
+void motorDirectionRight() {
   digitalWrite(PIN_1, HIGH);
   digitalWrite(PIN_2, LOW);
 
@@ -36,7 +93,7 @@ void motorDirectionLeft() {
 }
 
 // motor turn right function
-void motorDirectionRight() {
+void motorDirectionLeft() {
   digitalWrite(PIN_1, LOW);
   digitalWrite(PIN_2, HIGH);
 
@@ -51,33 +108,4 @@ void motorStop() {
 
   digitalWrite(PIN_3, LOW);
   digitalWrite(PIN_4, LOW);
-}
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(irPinLeft, INPUT);
-  pinMode(irPinRight, INPUT);
-}
-
-void loop() {
-  valueLeft = digitalRead(irPinLeft);
-  Serial.print("left: ");
-  Serial.println(valueLeft);
-
-  valueRight = digitalRead(irPinRight);
-  Serial.print("right: ");
-  Serial.println(valueRight);
-
-  if (valueLeft == 0 && valueRight == 0) {
-    motorDirectionForward();
-
-  } else if (valueLeft == 1 && valueRight == 0) {
-    motorDirectionRight();
-
-  } else if (valueLeft == 0 && valueRight == 1) {
-    motorDirectionLeft();
-
-  } else if (valueLeft == 1 && valueRight == 1) {
-    motorStop();
-  }
 }
--- ultrasoon_zonder_esp/ultrasoon_zonder_esp.ino
+++ ultrasoon_zonder_esp/ultrasoon_zonder_esp.ino
@@ -1,21 +1,99 @@
-int trigPinLinks = 13;
-int echoPinLinks = 12;
-int trigPinRechts = 8;
-int echoPinRechts = 9;
-int trigPinVoor = 10;
-int echoPinVoor = 11;
+// ultrasoon sensor pins
+#define TRIG_PIN_LEFT 8
+#define ECHO_PIN_LEFT 9
+#define TRIG_PIN_RIGHT 10
+#define ECHO_PIN_RIGHT 11
+#define TRIG_PIN_FRONT 12
+#define ECHO_PIN_FRONT 13
 
-long durationVoor;
-int distanceVoor;
-long durationLinks;
-int distanceLinks;
-long durationRechts;
-int distanceRechts;
+long durationVoor = 0;
+int distanceVoor = 0;
+long durationLinks = 0;
+int distanceLinks = 0;
+long durationRechts = 0;
+int distanceRechts = 0;
 
+// motor pins
 #define PIN_1 2
 #define PIN_2 3
 #define PIN_3 4
 #define PIN_4 5
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIN_1, OUTPUT);
+  pinMode(PIN_2, OUTPUT);
+  pinMode(PIN_3, OUTPUT);
+  pinMode(PIN_4, OUTPUT);
+
+  pinMode(TRIG_PIN_LEFT, OUTPUT);
+  pinMode(ECHO_PIN_LEFT, INPUT);
+  pinMode(TRIG_PIN_RIGHT, OUTPUT);
+  pinMode(ECHO_PIN_RIGHT, INPUT);
+  pinMode(TRIG_PIN_FRONT, OUTPUT);
+  pinMode(ECHO_PIN_FRONT, INPUT);
+}
+
+void loop() {
+  digitalWrite(TRIG_PIN_FRONT, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN_FRONT, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN_FRONT, LOW);
+
+  durationVoor = pulseIn(ECHO_PIN_FRONT, HIGH);
+  distanceVoor = (durationVoor / 2) / 29.1;;
+  Serial.print("voor: ");
+  Serial.println(distanceVoor);
+
+  digitalWrite(TRIG_PIN_LEFT, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN_LEFT, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN_LEFT, LOW);
+
+  durationLinks = pulseIn(ECHO_PIN_LEFT, HIGH);
+  distanceLinks = (durationLinks / 2) / 29.1;;
+  Serial.print("links: ");
+  Serial.println(distanceLinks);
+
+  digitalWrite(TRIG_PIN_RIGHT, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN_RIGHT, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN_RIGHT, LOW);
+
+  durationRechts = pulseIn(ECHO_PIN_RIGHT, HIGH);
+  distanceRechts = (durationRechts / 2) / 29.1;
+  Serial.print("rechts: ");
+  Serial.println(distanceRechts);
+
+  if (distanceVoor > 20 && distanceRechts > 20 && distanceLinks > 20 && valueLeft == 0 && valueRight == 0) {
+    motorDirectionForward();
+
+  } else if (distanceVoor <= 20 && distanceLinks <= 20 && valueLeft == 0 && valueRight == 1) {
+    motorDirectionLeft();
+
+  } else if (distanceVoor <= 20 && distanceRechts <= 20 && valueLeft == 1 && valueRight == 0) {
+    motorDirectionRight();
+
+  } else if (distanceLinks <= 20 && distanceRechts <= 20) {
+    motorDirectionForward();
+
+  } else if (distanceVoor <= 20) {
+    motorDirectionLeft();
+
+  } else if (distanceRechts <= 20) {
+    motorDirectionRight();
+
+  } else if (distanceLinks <= 20) {
+    motorDirectionLeft();
+
+  } else {
+    motorStop();
+  }
+
+}
 
 // motor forward function
 void motorDirectionForward() {
@@ -60,88 +138,4 @@ void motorStop() {
 
   digitalWrite(PIN_3, LOW);
   digitalWrite(PIN_4, LOW);
-}
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(PIN_1, OUTPUT);
-  pinMode(PIN_2, OUTPUT);
-  pinMode(PIN_3, OUTPUT);
-  pinMode(PIN_4, OUTPUT);
-
-  pinMode(trigPinLinks, OUTPUT);
-  pinMode(echoPinLinks, INPUT);
-
-  pinMode(trigPinRechts, OUTPUT);
-  pinMode(echoPinRechts, INPUT);
-
-  pinMode(trigPinVoor, OUTPUT);
-  pinMode(echoPinVoor, INPUT);
-}
-
-void loop() {
-  digitalWrite(trigPinVoor, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPinVoor, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPinVoor, LOW);
-
-  durationVoor = pulseIn(echoPinVoor, HIGH);
-  distanceVoor = durationVoor * 0.034 / 2;
-
-  Serial.print("Distance Voor: ");
-  Serial.println(distanceVoor);
-
-
-  digitalWrite(trigPinLinks, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPinLinks, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPinLinks, LOW);
-
-  durationLinks = pulseIn(echoPinLinks, HIGH);
-  distanceLinks = durationLinks * 0.034 / 2;
-
-  Serial.print("Distance Links: ");
-  Serial.println(distanceLinks);
-
-  digitalWrite(trigPinRechts, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPinRechts, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPinRechts, LOW);
-
-  durationRechts = pulseIn(echoPinRechts, HIGH);
-  distanceRechts = durationRechts * 0.034 / 2;
-
-  Serial.print("Distance Rechts: ");
-  Serial.println(distanceRechts);
-
-  if (distanceVoor > 20 && distanceRechts > 20 && distanceLinks > 20 ) {
-    motorDirectionForward();
-
-  } else if (distanceVoor < 15 && distanceLinks < 15) {
-    motorDirectionLeft();
-
-  } else if (distanceVoor < 15 && distanceRechts < 15) {
-    motorDirectionRight();
-
-  } else if (distanceLinks < 15 && distanceRechts < 15) {
-    motorDirectionForward();
-
-  } else if (distanceVoor < 15) {
-    motorDirectionRight();
-
-  } else if (distanceRechts < 15) {
-    motorDirectionLeft();
-
-  } else if (distanceLinks < 15) {
-    motorDirectionRight();
-
-  } else {
-    motorStop();
-    delay(500);
-    motorDirectionRight();
-    delay(1000);
-  }
 }
